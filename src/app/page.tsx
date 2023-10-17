e6645ee050848f265b48f261a724a59a60192f53
linikerdg <liniker_11@hotmail.com>
2023-10-16 23:54:43 -0300

    only spells

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
 export default function Home() {
-	return <div className='bg-sky-200 w-screen'>Home</div>;
+	return (
+		<div className='bg-midnight w-screen h-screen p-4'>
+			<h1 className='mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl'>
+				Sai daqui... Vai la ver as Spells!
+			</h1>
+		</div>
+	);
 }
--- src/app/tests/page.tsx
+++ src/app/tests/page.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function Tests() {
-	return <div>Testes</div>;
+	return (
+		<div className='bg-midnight w-screen h-screen p-4'>
+			<h1 className='text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl'>
+				Eu falei. . . AS SPELLS!!!!!
+			</h1>
+		</div>
+	);
 }
